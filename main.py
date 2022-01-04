c7f4d5ecdba2a563d8addd60141371cc08fa99ff
Cornholius <y.layshkin@gmail.com>
2022-01-04 14:57:04 +0300

    перестала работать оплата

--- main.py
+++ main.py
@@ -4,9 +4,9 @@ from aiogram.dispatcher.storage import FSMContext
 from aiogram.types import message_id
 from aiogram.dispatcher.filters.state import State, StatesGroup
 from aiogram.contrib.fsm_storage.memory import MemoryStorage
-from aiogram.types.message import ContentType
+from aiogram.types.message import ContentType, Message
 import markups as nav
-from task import Task
+from states import Task, Payment
 from db import Database
 import json
 
@@ -23,7 +23,18 @@ storage = MemoryStorage()
 dp = Dispatcher(bot, storage=storage)
 db = Database()
 
+@dp.pre_checkout_query_handler()
+async def pre_checkout(pre_checkout_query: types.PreCheckoutQuery):
+    print(pre_checkout_query)
+    await bot.answer_pre_checkout_query(pre_checkout_query.id, ok=True)
 
+
+@dp.message_handler(content_types=ContentType.SUCCESSFUL_PAYMENT)
+async def process_pay(message: types.Message):
+    print(message['total_amount'])
+    if message.successful_payment.invoice_payload == 'test_payload':
+        await bot.send_message(message.from_user.id, 'Оплата прошла успешно!')
+        
 @dp.message_handler(commands=['start'])
 async def roleMenu(message: types.Message):
     await bot.delete_message(message.chat.id, message.message_id)
@@ -41,49 +52,45 @@ async def mainMenuCustomer(message: types.Message):
     await bot.send_message(message.from_user.id, description, reply_markup=nav.customerMenu)
 
 
-@dp.message_handler()
-async def replenish_balance(message: types.Message):
-    if message.text == 'Пополнить баланс':
-        await bot.delete_message(message.chat.id, message.message_id)
-        await bot.send_invoice(
-            chat_id=message.chat.id,
-            title="Пополнение счёта",
-            description="test description",
-            payload="test_payload",
-            provider_token=test_payload_token,
-            currency="RUB",
-            start_parameter="test_bot",
-            prices=[{"label": "Руб", "amount": 1000}]
-        )
 
-@dp.pre_checkout_query_handler()
-async def pre_checkout(pre_checkout_query: types.PreCheckoutQuery):
-    await bot.answer_pre_checkout_query(pre_checkout_query.id, ok=True)
 
-@dp.message_handler(content_types=ContentType.SUCCESSFUL_PAYMENT)
-async def process_pay(message: types.Message):
-    if message.successful_payment.invoice_payload == 'test_payload':
-        await bot.send_message(message.from_user.id, 'Оплата прошла успешно!')
 
-@dp.message_handler()
+# узнаём баланс
 async def check_balance(message: types.Message):
-    if message.text == 'Узнать баланс':
         await bot.delete_message(message.chat.id, message.message_id)
         balance = db.check_balance(message.from_user.id)
         await bot.send_message(message.from_user.id, f'<b>Ваш баланс {balance} руб.</b>')
 
 
+#  пополняем баланс
+async def replenish_balance(message: types.Message):
+    await bot.delete_message(message.chat.id, message.message_id)
+    await bot.send_message(message.from_user.id, Payment.question1)
+    await Payment.answer1.set()
 
 
+@dp.message_handler(state=Payment.answer1)
+async def send_invoice(message: types.Message, state: FSMContext):
+    async with state.proxy() as data:
+        data["answer1"] = message.text
+    await bot.send_invoice(
+        chat_id=message.chat.id,
+        title="Пополнение счёта",
+        description="test description",
+        payload="test_payload",
+        provider_token=test_payload_token,
+        currency="RUB",
+        start_parameter="test_bot",
+        prices=[{"label": "Руб", "amount": int(data["answer1"]) * 100}]
+    )
+    print('>>>>> Send invoice')
 
 
-# --- Начало составления объявления ---
-@dp.message_handler()
+# размещаем объявление
 async def enter_task(message: types.Message):
-    if message.text == 'Разместить объявление':
-        await bot.delete_message(message.chat.id, message.message_id)
-        await bot.send_message(message.from_user.id, Task.question1)
-        await Task.answer1.set()
+    await bot.delete_message(message.chat.id, message.message_id)
+    await bot.send_message(message.from_user.id, Task.question1)
+    await Task.answer1.set()
 
 @dp.message_handler(state=Task.answer1)
 async def answer_1(message: types.Message, state: FSMContext):
@@ -111,7 +118,16 @@ async def answer_3(message: types.Message, state: FSMContext):
     await bot.send_message(message.from_user.id, Task.task_complete)
     await bot.send_message(-1001773059385, task_message)
     await state.finish()
-# --- Конец составления объявления ---
 
 
+# главное меню
+@dp.message_handler()
+async def mainMenu(message: types.Message):
+    if message.text == 'Узнать баланс':
+        await check_balance(message)
+    elif message.text == 'Пополнить баланс':
+        await replenish_balance(message)
+    elif message.text == 'Разместить объявление':
+        await enter_task(message)
+    
 executor.start_polling(dp, skip_updates=True)
